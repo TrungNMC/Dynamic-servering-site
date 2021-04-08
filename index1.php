9e29d7415c3e8312e8616b35d455704a89eb09e5
NMCT <67629495+NMTC-K59@users.noreply.github.com>
2021-04-08 08:02:23 +0700

    trung da dang

---
+++ index1.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dynamic Serving Site</title>
+    <link href="style1.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+<body>
+<header>
+        <div>
+            <ul>
+                <li><i class="fa fa-bars" aria-hidden="true"></i></a></li>
+                <li class="all"><a href="#">Tất cả</a></li>
+                <li><a href="#">Hình ảnh</a></li>
+                <li><a href="#"><i class="fa fa-th" aria-hidden="true"></i></a></li>
+                <li><a href="#"><button class="login">Đăng nhập</button></a></li>
+            </ul>
+        </div>
+        <article id="google">
+            <img alt="Google" height="60" src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" width="200" data-atf="1">
+        </article>
+        <article id="search">
+            <div class="label">
+                <ul>
+                    <li style="width: 4%;"><img class="sear" src="img/search.png" alt=""></i></li>
+                    <li style="width: 90%;padding-top: 2px;"><input type="text" class="textsearch"></li>
+                    <li style="width: 4%;"><img class="mic" src="img/mic.png" alt=""></li>
+                </ul>
+            </div>
+        </article>
+        <article id="language">
+            <p>Google có các thứ tiếng:</p>
+            <ul class="languages">
+                <li><a href="#">English</li>
+                <li><a href="#">Français</li>
+                <li><a href="#">中文（繁體）</li>
+            </ul>
+        </article>
+        
+    </header>
+    <div class="footer">
+        <div class="row1"> 
+            <ul><li><a href="#">Việt Nam</a></li></ul>
+        </div>
+        <div class="row2">
+            <ul>
+                <li><a href="#">Cài đặt</a></li>
+                <li><a href="#">Bảo mật</a></li>
+                <li><a href="#">Điều khoản</a></li>
+            </ul>
+        </div>
+        <div class="row3">
+            <ul>
+                <li><a href="#">Quảng cáo</a></li>
+                <li><a href="#">Doanh nghiệp</a></li>
+                <li><a href="#">Giới thiệu</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
